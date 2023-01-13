46d4a25ce92bc26dfbafa3788e53477f9769b88e
Gavin Aldred <Gavin.Aldred@nationaltimbergroup.com>
2023-01-13 21:37:42 +0000

    pushes starter code and file structure

---
+++ assets/js/questions.js
@@ -0,0 +1,127 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
+        correctAnswer: "Hyper Text Markup Language"
+      },
+
+];
+
+
+
+// What does HTML stand for?
+// A. 
+// Hyper Trainer Marking Language
+
+// B. 
+// Hyper Text Marketing Language
+
+// C. 
+// Hyper Text Markup Language
+
+// D. 
+// Hyper Text Markup Leveler
+
+
+
+// What does CSS stand for?
+
+
+
+// Q 2 - Which of the following is a way to associate styles with your HTML document?
+
+// A - External CSS - The Element
+
+// B - Imported CSS - @import Rule
+
+// C - Both of the above.
+
+// D - None of the above.
+
+
+// Q 5 - Which of the following property is used to increase or decrease the size of a font?
+
+// A - font-size
+
+// B - font
+
+// C - font-variant
+
+// D - font-weight
+
+
+
+// Inside which HTML element do we put the JavaScript?
+
+
+// <script>
+// <js>
+// <javascript>
+// <code>
+
+// How do you write "Hello World" in an alert box?
+
+//  msg("Hello World")
+//  alertBox("Hello World");
+//  msgBox("Hello World");
+//  alert("Hello World");
+
+
+
+
+
+
+// // A start button function
+//     // loads Q&A
+//     // starts timer
+// //  timer starts
+//     // Timer function
+//     // countdown
+//     // display
+//     // get game stop
+//     // stop at zero
+// // 
+    
+//     //Timer connected to:
+//         // Game start f
+//         // Game stop f
+//         // timer display
+//         // Incorrect answer - time deduction
+    
+// //gamestart function
+//     // trigger from button
+//     // start timer
+//     // call show questions and choices function
+//     // hide submit button
+//     // hide instructions
+// // toggle class attribute hide
+//     //  start-screen
+//     // questions container 
+// //Gamestop function
+//     // trigger from
+//         //  last question 
+//         // timer zero
+//     // announces score
+//     // gets initials
+//     // post to local storage
+//     // show submit button
+//     // show instructions
+    
+// // highscore function
+//     // on load, update from local storage
+//     // rank by score 
+//     // activate clear highscores button
+    
+// // clear highscore function
+//     // clears LS
+// // Q&A function 
+//     // get q&A
+//     // display
+//     // interactive
+//     // on click
+//         // show choice
+//         // show correct answer
+//         // clear
+//         // get new questions
+// // choice display
+// // evaluate choice function
